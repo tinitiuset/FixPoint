ee4562ed9b4c33384d6ff1fb8d291e32627e16cd
darkbazory <73275943+darkbazory@users.noreply.github.com>
2021-10-18 09:00:04 +0200

    vuelta a version anterior

--- web/JS/Menu.js
+++ web/JS/Menu.js
@@ -1,5 +1,4 @@
-window.onload = function() {
-    console.log("Ejecutando")
+ console.log("Ejecutando")
     function cambiarClase(){ /*llamamos a la funcion cambiarclase*/
         let siteNav = document.getElementById('site-nav'); /* Declaramos la variable siteNave*/ 
             siteNav.classList.toggle('site-nav-open'); /**Aqui añadimos la clase que queramos que añada o quite */
@@ -8,4 +7,3 @@ window.onload = function() {
             menuOpen.classList.toggle('menu-open');
 
     }
-}
