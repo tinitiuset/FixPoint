eb30834c92ed35fd290953c01877304c03cd789b
darkbazory <73275943+darkbazory@users.noreply.github.com>
2021-10-18 08:27:42 +0200

    header desktop finalizado

--- web/JS/Menu.js
+++ web/JS/Menu.js
@@ -1,11 +1,11 @@
-console.log("Ejecutando")
-function cambiarClase(){ /*llamamos a la funcion cambiarclase*/
-    let siteNav = document.getElementById('site-nav'); /* Declaramos la variable siteNave*/ 
-        siteNav.classList.toggle('site-nav-open'); /**Aqui añadimos la clase que queramos que añada o quite */
-    let Login = document.getElementById('Login');
-        
-    let menuOpen = document.getElementById('menu-toggle');/** unimos el menu open con toggle para que al hacer
-    click haga la transformación que pusimos en el css */
-        menuOpen.classList.toggle('menu-open');
+window.onload = function() {
+    console.log("Ejecutando")
+    function cambiarClase(){ /*llamamos a la funcion cambiarclase*/
+        let siteNav = document.getElementById('site-nav'); /* Declaramos la variable siteNave*/ 
+            siteNav.classList.toggle('site-nav-open'); /**Aqui añadimos la clase que queramos que añada o quite */
+        let menuOpen = document.getElementById('menu-toggle');/** unimos el menu open con toggle para que al hacer
+        click haga la transformación que pusimos en el css */
+            menuOpen.classList.toggle('menu-open');
 
+    }
 }
