f1c53bdb07d0b3390570054067d9aaede3426227
4rkaitz <arkaitz.garot@gmail.com>
2021-10-19 09:56:40 +0200

    Update Connection.php

--- web/Connection.php
+++ web/Connection.php
@@ -1,43 +1,11 @@
 <?php
-
-namespace Grupo3\FixPoint;
-
 require __DIR__ . '/../vendor/autoload.php';
 
-use Dotenv;
-use PDO;
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/../");
+$dotenv->load();
 
-class Connection
-{
-    private static PDO $connection;
-
-    public static function __constructStatic()
-    {
-        try {
-
-            $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
-            $dotenv->load();
-
-            $db_host = $_SERVER['DB_HOST'];
-            $db_name = $_SERVER['DB_NAME'];
-            $u_name = $_SERVER['DB_USER'];
-            $u_pass = $_SERVER['DB_PASS'];
-            $port = $_SERVER['DB_PORT'];
-            var_dump($db_host);
-            $conn = new PDO("mysql:host=$db_host;dbname=$db_name",
-                $u_name, $u_pass);
-            var_dump($conn);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            self::$connection = $conn;
-        } catch (PDOException $e) {
-            echo "Conexion fallida: " . $e->getMessage();
-        }
-    }
-
-    static function executeQuery($query = null)
-    {
-       return self::$connection->query($query);
-    }
-}
-Connection::__constructStatic();
+echo($_SERVER['DB_HOST']);
+echo($_SERVER['DB_USER']);
+echo($_SERVER['DB_PASS']);
+echo($_SERVER['DB_PORT']);
 ?>
