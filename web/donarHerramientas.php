b836fbdbdf9a9e0eb1ca8c43d7f37c4fbc978121
darkbazory <73275943+darkbazory@users.noreply.github.com>
2021-11-02 14:21:20 +0100

    donar herramientas hecho - falta comprobar form

--- web/donarHerramientas.php
+++ web/donarHerramientas.php
@@ -20,65 +20,87 @@ $args = [
 ];
 
 function getContent () {
+
+    if (isset($_POST['Enviar'])) {
+        if (!empty($_POST['nombre']) && !empty($_POST['apellido']) && !empty($_POST['email']) && !empty($_POST[donacion])) {
+            $name = $_POST['nombre'];
+            $apellido = $_POST['apellido'];
+            $email = $_POST['email'];
+            $msg = $_POST['donacion'];
+            $header = "From: noreply@example.com" . "r\n";
+            $header.= "Reply-To: noreply@example.com" . "r\n";
+            $header.= "X-Mailer: PHP/". phpversion();
+            $mail = @mail($mail, $nombre, $apellido, $msg, $header);
+            if ($mail) {
+                echo "<h4> El email se ha enviado correctamente</h4>";
+            }
+    
+    
+        }
+        
+    }
+    
     $content= '
     <h1 id="tituloTools">¡Necesitamos herramientas!</h1>
-    <div class="contenedor">
+    <div class="container">
         <div Class="imagen">
             <img class="imgHerramientas" src="./img/herramienta.jpeg">
         </div>
         <div class="parrafos">
-        <p class="textDonar">Para mantener la biblioteca de herramientas de FixPoint en funcionamiento,
-            pedimos a as personas que donen herramientas</p>
-        <p class="textDonar">Estamos muy interesados ​​en donaciones de herramientas manuales, eléctricas, 
-            de jardín, equipos de pintura, escaleras y maquinaria para trabajar la madera. 
-            También nos gustaría herramientas más especializadas para trabajos como enyesado,
-            alicatado y reparación de bicicletas. Para ser honesto, estamos abiertos a ofertas
-            de cualquier otra herramienta que le gustaría donar.</p>
-        <p class="textDonar">Idealmente, nos gustaría herramientas que estén en buenas condiciones para estar 
-            listas para usar, pero también consideraremos herramientas que necesitan reparaciones
-            para poder practicar en el taller de despiece</p>
-        <p class="textDonar">Si tienes algo para donar, ¡nos encantaría saberlo!
-            Solo completa el formulario de abajo. ¡Gracias!</p>
+            <p class="textDonar">Para mantener la biblioteca de herramientas de FixPoint en funcionamiento,
+                pedimos a las personas que donen herramientas</p>
+            <p class="textDonar">Estamos muy interesados ​​en donaciones de herramientas manuales, eléctricas, 
+                de jardín, equipos de pintura, escaleras y maquinaria para trabajar la madera. 
+                También nos gustaría herramientas más especializadas para trabajos como enyesado,
+                alicatado y reparación de bicicletas. Para ser honesto, estamos abiertos a ofertas
+                de cualquier otra herramienta que le gustaría donar.</p>
+            <p class="textDonar">Idealmente, nos gustaría herramientas que estén en buenas condiciones para estar 
+                listas para usar, pero también consideraremos herramientas que necesitan reparaciones
+                para poder practicar en el taller de despiece</p>
+            <p class="textDonar">Si tienes algo para donar, ¡nos encantaría saberlo!
+                Solo completa el formulario de abajo y recibiras un correo con las instrucciones. ¡Gracias!</p>
         </div>
         <div class="formulario">
-        <form action="" method="post" id="formularioDonacion">
-            <div class="contenedor">
-                <fieldset>
-                    <legend class="title">
-                        <Font>Nombre</font>
-                        <span>*</span>
-                    </legend>
-                    <div class="nombreApellido">
-                        <label class="nombre">
-                            <input class="inputForm" type="text" name="nombre" id="nombre"><br>
-                            <span class="nombre">Primer nombre</span>
+            <form action="" method="post" id="formularioDonacion">
+                <div class="contenedor">
+                    <fieldset>
+                        <legend class="title">
+                            <Font>Nombre</font>
+                            <span>*</span>
+                        </legend>
+                        <div class="nombreApellido">
+                            <label class="nombre">
+                                <input class="inputForm" type="text" name="nombre" id="nombre"><br>
+                                <span class="nombre">Primer nombre</span>
+                            </label>
+                        </div>
+                        <div class="nombreApellido">
+                            <label class="nombre">
+                                <input class="inputForm" type="text" name="apellido" id="apellido"><br>
+                                <span class="nombre">Apellido</span>
+                            </label>
+                        </div>
+                    </fieldset>
+                    <div>
+                        <label class="titulo">Correo electrónico
+                            <span>*</span>
+                            <input class="inputForm" type="text" name="email">
                         </label>
                     </div>
-                    <div class="nombreApellido">
-                        <label class="nombre">
-                            <input class="inputForm" type="text" name="apellido" id="apellido"><br>
-                            <span class="nombre">Apellido</span>
+                    <div>
+                        <label class="titulo">Número de teléfono
+                            <input class="inputForm" type="text" name="telefono">
                         </label>
                     </div>
-                </fieldset>
-                <div>
-                    <label class="titulo">Correo electrónico
-                        <span>*</span>
-                        <input class="inputForm" type="text" name="email">
-                    </label>
-                </div>
-                <div>
-                    <label class="titulo">Número de teléfono
-                        <input class="inputForm" type="text" name="telefono">
-                    </label>
-                </div>
-                <div>
-                    <label class="titulo">¿Qué le gustaría donar?
-                        <span>*</span>
-                        <textarea id="textArea" name="donación"></textarea>
-                    </label>
-                </div>
-        </form>
+                    <div>
+                        <label class="titulo">¿Qué le gustaría donar?
+                            <span>*</span>
+                            <textarea id="textArea" name="donacion"></textarea>
+                            <input class="btnEnviar" type="submit" value="Enviar">
+                        </label>
+                    </div>
+                </div>    
+            </form>
         </div>
     </div>
     ';
