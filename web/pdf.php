c9b0df4800842ff51709436ecc01772643e1ca12
tinitiuset <tinitiuset@gmail.com>
2021-11-15 11:43:59 +0100

    Ahora se guardan las guias pero no los pasos.

--- web/pdf.php
+++ web/pdf.php
@@ -4,14 +4,13 @@ use Mpdf\Mpdf;
 use Mpdf\MpdfException;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
 if (isset($_POST['html'])) {
     try {
         $mpdf = new Mpdf([
             'mode' => 'c',
             'tempDir' => __DIR__ . '/pdf'
         ]);
-        $mpdf->WriteHTML($_POST['html']);
+        $mpdf->WriteHTML(base64_decode($_POST['html']));
         $mpdf->Output();
     } catch (MpdfException $e) {
         echo($e);
