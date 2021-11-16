30720c04962cbc4b5ed4412c6561b6fb11be64a4
tinitiuset <tinitiuset@gmail.com>
2021-11-16 10:41:19 +0100

    Guias por ID

--- web/guia.php
+++ web/guia.php
@@ -33,7 +33,7 @@ function getContent()
 {
     try {
         $guia = new guiaDespiece('', '', '', '', '', '');
-        $guia->getGuiaDespiece(3); /*$_POST['id']*/
+        $guia->getGuiaDespiece($_POST['id']);
 
         $creadorGuia = new creadorguia();
         $creadorGuia->getCreadorGuiaByNumFicha($guia->getNumFicha());
