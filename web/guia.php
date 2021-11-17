0fa14e21baf4d53da347eb2d4815c5e0766cf4bc
tinitiuset <tinitiuset@gmail.com>
2021-11-17 08:06:41 +0100

    Enlaces a Guias arreglados.

--- web/guia.php
+++ web/guia.php
@@ -33,7 +33,6 @@ function getContent()
 {
     try {
         $guia = new guiaDespiece('', '', '', '', '', '');
-        $guia->getGuiaDespiece($_POST['id']);
 
         $creadorGuia = new creadorguia();
         $creadorGuia->getCreadorGuiaByNumFicha($guia->getNumFicha());
